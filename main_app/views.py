ad1a8f22b0437b4aea49b29fae0e5764ead3dce8
Alice Stancu <alice.stancu@gmail.com>
2023-05-27 19:10:08 -0400

    create game collector

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+#usually a model is used
+games = [
+  {'name': 'The Legend of Zelda: A Link to the Past', 'console': 'Super Famicom', 'genre': 'action-adventure', 'released': '1991'},
+
+  {'name': 'Chrono Trigger', 'console': 'Super Famicom', 'genre': 'JRPG', 'released': '1995'},
+
+  {'name': 'Donkey Kong Country 3', 'console': 'Super Famicom', 'genre': 'platform', 'released': '1996'},
+]
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def games_index(request):
+    return render(request, 'games/index.html', {      'games': games
+    })

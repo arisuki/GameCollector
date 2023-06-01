fc04d0fc967a9038c76797897f086c17865c3270
Alice Stancu <alice.stancu@gmail.com>
2023-05-31 22:42:14 -0400

    part 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    finished = models.BooleanField(default=False)
+    details = models.TextField(max_length=500)
+    console = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    released = models.IntegerField()
+
+
+    def __str__(self):
+        return f'{self.name}({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'game_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
-
-#usually a model is used
-games = [
-  {'name': 'The Legend of Zelda: A Link to the Past', 'console': 'Super Famicom', 'genre': 'action-adventure', 'released': '1991'},
-
-  {'name': 'Chrono Trigger', 'console': 'Super Famicom', 'genre': 'JRPG', 'released': '1995'},
-
-  {'name': 'Donkey Kong Country 3', 'console': 'Super Famicom', 'genre': 'platform', 'released': '1996'},
-]
-
-# Create your views here.
+from .models import Game
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def home(request):
     return render(request, 'home.html')
@@ -18,5 +9,22 @@ def about(request):
     return render(request, 'about.html')
 
 def games_index(request):
-    return render(request, 'games/index.html', {      'games': games
-    })
+    games = Game.objects.all()
+    return render(request, 'games/index.html', {'games': games})
+
+def games_detail(request, game_id):
+    game = Game.objects.get(id=game_id)
+    return render(request, 'games/detail.html', {'game': game})
+
+class GameCreate(CreateView):
+    model = Game
+    fields = '__all__'
+
+class GameUpdate(UpdateView):
+    model = Game
+    fields = '__all__'
+    # ['finished', 'detail', 'console', 'genre', 'released' ]
+
+class GameDelete(DeleteView):
+    model = Game
+    success_url = '/games'

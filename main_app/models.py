5e6b8df994b344129c7bc6cc2a21fcb952804055
Alice Stancu <alice.stancu@gmail.com>
2023-06-03 02:09:37 -0400

    styling game collector

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+TYPE = (
+    ('T', 'Thoughts'),
+    ('R', 'Review'),
+    ('P', 'Progress')
+)
+
 class Game(models.Model):
     name = models.CharField(max_length=100)
     finished = models.BooleanField(default=False)
@@ -17,3 +23,18 @@ class Game(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'game_id': self.id})
+
+class Note(models.Model):
+    date = models.DateField('posted on', auto_now_add=True)
+    content = models.CharField('content of note', max_length=500)
+    type = models.CharField(
+        'type of note',
+        max_length=1,
+        choices = TYPE,
+        default=TYPE[0][0],
+    )
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_type_display()} on {self.date}"
